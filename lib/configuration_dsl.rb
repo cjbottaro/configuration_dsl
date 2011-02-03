91436c5d969f73003fe3be44c84b69ed416e7655
Christopher J Bottaro <cjbottaro@alumni.cs.utexas.edu>
2011-02-02 21:50:28 -0600

    Refactored.

--- lib/configuration_dsl.rb
+++ lib/configuration_dsl.rb
@@ -1,5 +1,5 @@
 require "configuration_dsl/dsl"
-require "configuration_dsl/helper"
+require "configuration_dsl/impl"
 
 module ConfigurationDsl
   
@@ -8,28 +8,32 @@ module ConfigurationDsl
   VERSION = File.read(File.dirname(__FILE__)+"/../VERSION")
   
   def configure_with(configuration_module, &block)
-    helper = Helper.new(self)
-    settings = helper.initialize_settings
-    settings[:module] = configuration_module
-    settings[:callback] = block if block_given?
-    @configuration = helper.default_configuration
+    @configuration_dsl ||= Impl.new(self)
+    @configuration_dsl.module = configuration_module
+    @configuration_dsl.callback = block if block_given?
+    @configuration_dsl.default_configuration!
   end
   
   def configure(&block)
-    helper = Helper.new(self)
+    @configuration_dsl ||= Impl.new(self)
     
+    # Instance eval the block.
     if block_given?
-      raise Error, "cannot find configuration module" unless helper.module
+      _module = @configuration_dsl.find_module
+      raise Error, "cannot find configuration module" unless _module
       dsl = Dsl.new(configuration)
-      dsl.send(:extend, helper.module)
+      dsl.send(:extend, _module)
       dsl.instance_eval(&block)
     end
     
-    instance_eval(&helper.callback) if helper.callback
+    # Run the callback.
+    callback = @configuration_dsl.find_callback
+    instance_eval(&callback) if callback
   end
   
   def configuration
-    @configuration ||= Helper.new(self).inherit_configuration
+    @configuration_dsl ||= Impl.new(self)
+    @configuration_dsl.configuration ||= @configuration_dsl.inherit_configuration
   end
   
 end
---
+++ lib/configuration_dsl/impl.rb
@@ -0,0 +1,64 @@
+module ConfigurationDsl
+  class Impl
+    attr_accessor :module, :callback, :configuration
+    
+    def self.dup_struct(struct)
+      members = struct.members.collect{ |member| member.to_sym } # Normalize between Ruby versions.
+      values = struct.values.collect do |value|
+        if value.kind_of?(Class)
+          value
+        else
+          value.dup rescue value
+        end
+      end
+      Struct.new(*members).new(*values)
+    end
+    
+    def initialize(object)
+      @object = object
+    end
+    
+    def find_module
+      ancestors.each do |object|
+        next unless (impl = object.instance_variable_get(:@configuration_dsl))
+        return impl.module if impl.module
+      end
+      nil
+    end
+    
+    def find_callback
+      ancestors.each do |object|
+        next unless (impl = object.instance_variable_get(:@configuration_dsl))
+        return impl.callback if impl.callback
+      end
+      nil
+    end
+    
+    def find_configuration
+      ancestors.each do |object|
+        next unless (impl = object.instance_variable_get(:@configuration_dsl))
+        return impl.configuration if impl.configuration
+      end
+      nil
+    end
+    
+    def default_configuration
+      defaults = find_module.const_get(:DEFAULTS)
+      Struct.new(*defaults.keys).new(*defaults.values)
+    end
+    
+    def default_configuration!
+      @configuration = default_configuration
+    end
+    
+    def inherit_configuration
+      configuration = find_configuration
+      configuration and Impl.dup_struct(configuration)
+    end
+    
+    def ancestors
+      @object.respond_to?(:ancestors) ? @object.ancestors : [@object]
+    end
+    
+  end
+end
